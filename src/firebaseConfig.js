efe8b36548c35217fd79cebb878c032d0f004687
darshan15062002 <darshanjain15062002@gmail.com>
2023-01-13 08:48:57 +0530

    m s ds

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,22 +1,24 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 
-import { getAuth } from "firebase/auth";
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAfqhUCuraCCywdBmB0r5IY2Li5kXcMGFY",
-    authDomain: "fir-4574c.firebaseapp.com",
-    projectId: "fir-4574c",
-    storageBucket: "fir-4574c.appspot.com",
-    messagingSenderId: "1053071283939",
-    appId: "1:1053071283939:web:bd168a374db95d0a8557d6",
-    measurementId: "G-7MYFYC3Z9M"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
+// import { getAuth } from "firebase/auth";
 
 
-export { auth };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAfqhUCuraCCywdBmB0r5IY2Li5kXcMGFY",
+//     authDomain: "fir-4574c.firebaseapp.com",
+//     projectId: "fir-4574c",
+//     storageBucket: "fir-4574c.appspot.com",
+//     messagingSenderId: "1053071283939",
+//     appId: "1:1053071283939:web:bd168a374db95d0a8557d6",
+//     measurementId: "G-7MYFYC3Z9M"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// const auth = getAuth(app);
+
+
+// export { auth };
+
+
